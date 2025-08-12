7e424a19a08d84cac19c78899720121b46071bfa
Alparslan Tarkan <atarkan@hotmail.com>
2025-08-13 00:46:22 +0200

    bean lifecycle methods

---
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/sarender/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,28 @@
+package com.sarender.springcoredemo.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach{
+
+    public CricketCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+    //the init method
+    @PostConstruct
+    public void doMyStartupStuff(){
+        System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+    }
+    //the destroy method
+    @PreDestroy
+    public void doMyCleanupStuff(){
+        System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes";
+    }
+}
