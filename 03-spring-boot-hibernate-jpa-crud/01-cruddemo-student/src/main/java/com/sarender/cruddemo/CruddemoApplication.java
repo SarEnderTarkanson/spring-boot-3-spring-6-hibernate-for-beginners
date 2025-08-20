8b8dcee861b8d565b8d23de23cbbbb1a7feb39eb
Alparslan Tarkan <atarkan@hotmail.com>
2025-08-20 06:49:55 +0200

    primary keys

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/sarender/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/sarender/cruddemo/CruddemoApplication.java
@@ -17,8 +17,21 @@ public class CruddemoApplication {
     @Bean
     public CommandLineRunner commandLineRunner(StudentDao studentDao) {
         return runner -> {
-            createStudent(studentDao);
+            //createStudent(studentDao);
+            createMultipleStudents(studentDao);
         };
+    }
+
+    private void createMultipleStudents(StudentDao studentDao) {
+        System.out.println("Creating 3 student objects...");
+        Student tempStudent1 = new Student("Jane", "Doe", "atarkan2@hotmail.com");
+        Student tempStudent2 = new Student("Endo", "Doe", "atarkan3@hotmail.com");
+        Student tempStudent3 = new Student("Beyza", "Doe", "atarkan4@hotmail.com");
+
+        System.out.println("Saving the students...");
+        studentDao.save(tempStudent1);
+        studentDao.save(tempStudent2);
+        studentDao.save(tempStudent3);
     }
 
     private void createStudent(StudentDao studentDao) {
