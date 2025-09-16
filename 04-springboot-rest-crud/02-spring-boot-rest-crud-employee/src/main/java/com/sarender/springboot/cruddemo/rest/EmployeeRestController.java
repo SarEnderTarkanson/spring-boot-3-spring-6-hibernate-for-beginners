986a24ac8e05b87b8fcea54699874fc7aae7e63a
Alparslan Tarkan <atarkan@hotmail.com>
2025-09-16 23:47:11 +0200

    spring boot rest get single employee

--- 04-springboot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/sarender/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-springboot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/sarender/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,6 +3,7 @@ package com.sarender.springboot.cruddemo.rest;
 import com.sarender.springboot.cruddemo.entity.Employee;
 import com.sarender.springboot.cruddemo.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,17 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return theEmployee;
     }
 
 }
