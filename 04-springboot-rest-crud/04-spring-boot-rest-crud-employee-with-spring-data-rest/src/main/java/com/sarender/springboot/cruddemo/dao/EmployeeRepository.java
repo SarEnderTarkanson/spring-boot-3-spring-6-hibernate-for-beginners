270f686bd6fef01602c823d5b6acef31a0c64693
Alparslan Tarkan <atarkan@hotmail.com>
2025-09-29 17:15:31 +0200

    spring boot rest spring data rest configs and sorting

--- 04-springboot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/sarender/springboot/cruddemo/dao/EmployeeRepository.java
+++ 04-springboot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/sarender/springboot/cruddemo/dao/EmployeeRepository.java
@@ -3,6 +3,7 @@ package com.sarender.springboot.cruddemo.dao;
 import com.sarender.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
