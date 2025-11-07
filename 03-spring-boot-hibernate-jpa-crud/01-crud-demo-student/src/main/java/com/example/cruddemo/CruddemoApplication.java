6ed0db5ee078ec1ca6dc290dbd06f288aa6d08ba
Alparslan Tarkan <atarkan@hotmail.com>
2025-11-07 08:32:13 +0100

    querying objects with jpa 2

--- 03-spring-boot-hibernate-jpa-crud/01-crud-demo-student/src/main/java/com/example/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-crud-demo-student/src/main/java/com/example/cruddemo/CruddemoApplication.java
@@ -22,8 +22,18 @@ public class CruddemoApplication {
             //createStudent(studentDAO);
             //createMultipleStudents(studentDAO);
             //readStudent(studentDAO);
-            queryForStudents(studentDAO);
+            //queryForStudents(studentDAO);
+            queryForStudentsByLastName(studentDAO);
         };
+    }
+
+    private void queryForStudentsByLastName(StudentDAO studentDAO) {
+
+        List<Student> theStudents = studentDAO.findByLastName("Doe");
+
+        for (Student tempStudent : theStudents) {
+            System.out.println(tempStudent);
+        }
     }
 
     private void queryForStudents(StudentDAO studentDAO) {
