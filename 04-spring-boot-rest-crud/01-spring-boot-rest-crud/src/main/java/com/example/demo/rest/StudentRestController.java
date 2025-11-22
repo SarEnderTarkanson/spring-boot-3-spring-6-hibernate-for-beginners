bf6946cbd7c9fee5c30680c4394e0425ad73dbc3
Alparslan Tarkan <atarkan@hotmail.com>
2025-11-23 00:26:36 +0100

    spring boot rest path variables 1

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/example/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/example/demo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.example.demo.rest;
 
 import com.example.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,14 +13,21 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
+    private List<Student> theStudents;
 
-        List<Student> theStudents = new ArrayList<>();
+    @PostConstruct
+    public void loadData() {
+
+        theStudents = new ArrayList<>();
 
         theStudents.add(new Student("Poornima", "Patel"));
         theStudents.add(new Student("Mario", "Rossi"));
         theStudents.add(new Student("Mary", "Smith"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+
         return theStudents;
     }
 
